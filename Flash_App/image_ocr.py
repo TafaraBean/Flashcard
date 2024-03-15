352795d1421bfe8accb476541973f7dd46b84442
TafaraBean <kanyembat25@gmail.com>
2024-03-15 17:53:00 +0200

    image compression

--- Flash_App/image_ocr.py
+++ Flash_App/image_ocr.py
@@ -1,14 +1,31 @@
-# Flash_App/image_ocr.py
 import pytesseract
 from PIL import Image
 
-#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def process_image_for_ocr(image_file):
-    # Open the image file using PIL (Python Imaging Library)
+    # Open the image file
     image = Image.open(image_file)
-    
-    # Use pytesseract to perform OCR on the image and extract text
+
+    # Check for compression need
+    if image.size[0] * image.size[1] > 1024 * 1024:  # Multiply width and height
+        image = compress_image(image)
+
+
+    # Perform OCR
     text = pytesseract.image_to_string(image)
-    # Return the extracted text
+
     return text
+
+def compress_image(image):
+    # Resize image if needed
+    max_width = 1024  # Adjust as needed
+    if image.width > max_width:
+        image = image.resize((max_width, image.height * max_width // image.width), Image.LANCZOS)
+
+    # Save as JPEG with quality optimization
+    image.save("compressed_image.jpg", optimize=True, quality=85)
+
+    # Reload the compressed image
+    compressed_image = Image.open("compressed_image.jpg")
+    return compressed_image
