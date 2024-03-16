6b2b797f2e238ccc6263a44d5fc977f7ba979eec
TafaraBean <kanyembat25@gmail.com>
2024-03-16 23:18:37 +0200

    texify

--- Flash_App/image_ocr.py
+++ Flash_App/image_ocr.py
@@ -1,21 +1,30 @@
 import pytesseract
 from PIL import Image
+from texify.inference import batch_inference
+from texify.model.model import load_model
+from texify.model.processor import load_processor
+#from pix2tex.cli import LatexOCR
 
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def process_image_for_ocr(image_file):
     # Open the image file
+    model = load_model()
+    processor = load_processor()
     image = Image.open(image_file)
 
     # Check for compression need
-    if image.size[0] * image.size[1] > 1024 * 1024:  # Multiply width and height
-        image = compress_image(image)
+    #if image.size[0] * image.size[1] > 1024 * 1024:  # Multiply width and height
+       #image = compress_image(image)
 
+    #model = LatexOCR()
+    #text = print(model(image))
 
     # Perform OCR
-    text = pytesseract.image_to_string(image)
+    #text = pytesseract.image_to_string(image)
+    results = batch_inference([image],model,processor)
 
-    return text
+    return results
 
 def compress_image(image):
     # Resize image if needed
