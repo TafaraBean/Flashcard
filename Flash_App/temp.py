43edb055da2f40730e49cb541f1f1e8b7a1d9838
TafaraBean <kanyembat25@gmail.com>
2024-03-18 18:06:50 +0200

    new model t5 small

--- Flash_App/temp.py
+++ Flash_App/temp.py
@@ -1,9 +1,8 @@
 
-# pip install accelerate
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base", device_map="auto")
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-small", device_map="auto")
 
 input_text = "translate English to German: How old are you?"
 input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to("cuda")
