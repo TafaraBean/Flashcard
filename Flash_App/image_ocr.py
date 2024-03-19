5e02f7c47f9de39e04adc30771afb6815684441b
TafaraBean <kanyembat25@gmail.com>
2024-03-19 16:54:12 +0200

    revert to old tesseract model

--- Flash_App/image_ocr.py
+++ Flash_App/image_ocr.py
@@ -1,41 +1,30 @@
 import pytesseract
 from PIL import Image
-from texify.inference import batch_inference
-from texify.model.model import load_model
-from texify.model.processor import load_processor
-
 
 #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def process_image_for_ocr(image_file):
     # Open the image file
-    
-    model = load_model()
-    processor = load_processor()
     image = Image.open(image_file)
-    results = batch_inference([image], model, processor)
 
     # Check for compression need
-    # if image.size[0] * image.size[1] > 1024 * 1024:  # Multiply width and height
-    #     image = compress_image(image)
+    if image.size[0] * image.size[1] > 1024 * 1024:  # Multiply width and height
+        image = compress_image(image)
 
 
     # Perform OCR
-    #text = pytesseract.image_to_string(image)
+    text = pytesseract.image_to_string(image)
 
-    return results
+    return text
 
 def compress_image(image):
     # Resize image if needed
     max_width = 1024  # Adjust as needed
     if image.width > max_width:
-        image = image.resize((max_width, image.height * max_width // image.width), Image.LANCZOS)
+        image_return = image.resize((max_width, image.height * max_width // image.width), Image.LANCZOS)
 
     # Save as JPEG with quality optimization
-    image.save("compressed_image.jpg", optimize=True, quality=85)
-
-    # Reload the compressed image
-    compressed_image = Image.open("compressed_image.jpg")
-    return compressed_image
+    
+    return image_return
 
 
